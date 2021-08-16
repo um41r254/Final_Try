8f6ec1c65e7ce0ad7f548ec17f17bd2f6ee7c4c7
elgobyte <57178092+elgobyte@users.noreply.github.com>
2021-08-16 18:23:37 +0500

    Multi-View Recycler View

--- app/src/main/java/com/mid_banchers/final_try/MainActivity.java
+++ app/src/main/java/com/mid_banchers/final_try/MainActivity.java
@@ -6,15 +6,14 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 import com.mid_banchers.final_try.adapters.AdapterHome;
 import com.mid_banchers.final_try.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     TabAdapter tabAdapter;
-    VerticalRv verticalRv;
+    AdapterHome adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +21,35 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        verticalRv = new VerticalRv(this);
-        binding.include.rvlatestver.setAdapter(new AdapterHome());
-        binding.include.rvlatestver.setLayoutManager(new GridLayoutManager(this, 2));
-        binding.catPager.setUserInputEnabled(false);
+        adapter = new AdapterHome(this);
 
 
-        tabAdapter = new TabAdapter(this);
+        binding.include.rvlatestver.setAdapter(adapter);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+
+                switch (adapter.getItemViewType(position)) {
+                    case 0:
+                        return 2;
+                    case 1:
+                        return 2;
+                    case 2:
+                        return 2;
+                    default:
+                        return 1;
+                }
+            }
+        });
+
+
+        binding.include.rvlatestver.setLayoutManager(gridLayoutManager);
+        // binding.catPager.setUserInputEnabled(false);
+
+
+     /*   tabAdapter = new TabAdapter(this);
         binding.catPager.setAdapter(tabAdapter);
         new TabLayoutMediator(binding.cataTabs, binding.catPager, (TabLayout.Tab tab, int position) -> {
             if (position == 0) {
@@ -44,8 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 tab.setText("Bridal");
             }
 
-
-        }).attach();
+        }).attach();*/
 
     }
 }
--- app/src/main/java/com/mid_banchers/final_try/adapters/AdapterHome.java
+++ app/src/main/java/com/mid_banchers/final_try/adapters/AdapterHome.java
@@ -1,16 +1,29 @@
 package com.mid_banchers.final_try.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.mid_banchers.final_try.MainActivity;
 import com.mid_banchers.final_try.R;
+import com.mid_banchers.final_try.TabAdapter;
 
 public class AdapterHome extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    private final Context context;
+
+    public AdapterHome(MainActivity mainActivity) {
+        this.context = mainActivity;
+    }
 
     @NonNull
     @Override
@@ -18,8 +31,16 @@ public class AdapterHome extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
         if (viewType == 0) {
             View view = LayoutInflater.from(parent.getContext()).
-                    inflate(R.layout.catagories_rv_layout, parent, false);
+                    inflate(R.layout.recycler_0, parent, false);
+            return new HolderA(view);
+        } else if (viewType == 1) {
+            View view = LayoutInflater.from(parent.getContext()).
+                    inflate(R.layout.recycler_1, parent, false);
             return new HolderX(view);
+        } else if (viewType == 2) {
+            View view = LayoutInflater.from(parent.getContext()).
+                    inflate(R.layout.recycler_1_heading, parent, false);
+            return new HolderY(view);
         } else {
             View view = LayoutInflater.from(parent.getContext()).
                     inflate(R.layout.latest_rv_layout, parent, false);
@@ -35,16 +56,22 @@ public class AdapterHome extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public int getItemViewType(int position) {
 
-        if (position < 10) {
+        if (position == 0) {
             return 0;
-        } else {
+        } else if (position == 1) {
             return 1;
+        } else if (position == 2) {
+            return 2;
+        } else if (position > 2) {
+            return 3;
+        } else {
+            return -1;
         }
     }
 
     @Override
     public int getItemCount() {
-        return 120;
+        return 15;
     }
 
     public class HolderX extends RecyclerView.ViewHolder {
@@ -58,5 +85,36 @@ public class AdapterHome extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         public HolderY(@NonNull View itemView) {
             super(itemView);
         }
+    }
+
+    public class HolderA extends RecyclerView.ViewHolder {
+        ViewPager2 viewPager;
+        TabLayout tabLayout;
+
+        public HolderA(@NonNull View itemView) {
+            super(itemView);
+            viewPager = itemView.findViewById(R.id.cat_pager);
+            tabLayout = itemView.findViewById(R.id.cata_tabs);
+
+           /* TabAdapter tabAdapter = new TabAdapter((FragmentActivity) context);
+            viewPager.setAdapter(tabAdapter);
+
+            new TabLayoutMediator(tabLayout, viewPager, (TabLayout.Tab tab, int position) -> {
+                if (position == 0) {
+                    tab.setText("Light");
+                }
+                if (position == 1) {
+                    tab.setText("Dark");
+                }
+                if (position == 2) {
+                    tab.setText("Event");
+                }
+                if (position == 3) {
+                    tab.setText("Bridal");
+                }
+            }).attach();*/
+        }
+
+
     }
 }
